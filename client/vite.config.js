47b2eaa33576177c28a5eeb2c67688ec22d3d2d7
Vishrut99 <vishrutpatel50@gmail.com>
2025-09-27 11:13:44 +0530

    build: switch to esbuild minifier to avoid terser dependency

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext',
-    minify: 'terser'
+    minify: true
   },
   optimizeDeps: {
     include: ['react', 'react-dom'],
